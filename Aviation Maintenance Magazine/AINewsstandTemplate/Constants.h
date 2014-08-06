f409dacce30121c6d3218605c62c36a21b9418eb
Gregory Lampa <gregoryteach@gmail.com>
2014-08-06 11:02:33 +0200

    Added Parse app key and json files

--- Aviation Maintenance Magazine/AINewsstandTemplate/Constants.h
+++ Aviation Maintenance Magazine/AINewsstandTemplate/Constants.h
@@ -6,13 +6,15 @@
 //  Copyright (c) 2014 Aleksey Ivanov. All rights reserved.
 //
 
-#warning ADD app id and app key from http://parse.com
-#define PARSE_APP_ID @""
-#define PARSE_APP_KEY @""
+//Define Interface Idioms
+#define IDIOM    UI_USER_INTERFACE_IDIOM()
+#define IPAD     UIUserInterfaceIdiomPad
 
-#warning change URLs to your JSON files
-#define JSON_IPAD_URL @"https://googledrive.com/host/0B6E2Hn-m7yvAUUpUV3JTZ2Y2TFE/ipad.json"
-#define JSON_IPHONE_RUL @"https://googledrive.com/host/0B6E2Hn-m7yvAUUpUV3JTZ2Y2TFE/iphone.json"
+#define PARSE_APP_ID @"oJ7y3lMWwWc1TxPe2CYuGK8SpkDYh4fDGX76Lbtf"
+#define PARSE_APP_KEY @"fmLnJWJFyMtVmxdmcYTWyIiZTriXb7zAk7fRh6VN"
+
+#define JSON_IPAD_URL @"http://www.avm-mag.com/wp-content/ipad-app/ipad.json"
+#define JSON_IPHONE_RUL @"http://www.avm-mag.com/wp-content/ipad-app/iphone.json"
 
 #define DEV_MODE YES //set NO before publish in AppStore
 
